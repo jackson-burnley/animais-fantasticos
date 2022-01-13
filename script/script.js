4ecffcfeeaeadc05defabe5abd9a8e8cdda3467b
Jack <jacktech.system@gmail.com>
2022-01-13 13:15:50 -0300

    Animacao do scroll

--- script/script.js
+++ script/script.js
@@ -36,5 +36,41 @@ function initAccordion() {
   }
 }
 
+function initScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+function scrollAnimado() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+  animaScroll();
+  window.addEventListener("scroll", animaScroll);
+}
+
 initTabNav();
 initAccordion();
+initScroll();
+scrollAnimado();

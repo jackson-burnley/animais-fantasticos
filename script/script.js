9cc721688b606b3e61777c4ad856982a995b50a2
Jack <jacktech.system@gmail.com>
2022-01-13 12:43:05 -0300

    Animacao do faq dt

--- script/script.js
+++ script/script.js
@@ -1,21 +1,40 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+//Função para controlar a tab do nav
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-  //Função para ativar o texto a depender da imagem que clicar
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", function () {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-
-  tabMenu.forEach((item, index) => {
-    item.addEventListener("click", function () {
-      activeTab(index);
-    });
-  });
 }
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.add("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initTabNav();
+initAccordion();

9adb27d71b8d855260aeadb5495a08acf72939d3
Jack <jacktech.system@gmail.com>
2022-01-31 22:52:46 -0300

    Add dataset e modulos

--- script/script.js
+++ script/script.js
@@ -1,76 +1,9 @@
-//Função para controlar a tab do nav
-function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
-
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      tabContent[index].classList.add("ativo");
-    }
-
-    tabMenu.forEach((item, index) => {
-      item.addEventListener("click", function () {
-        activeTab(index);
-      });
-    });
-  }
-}
-
-function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
-  if (accordionList.length) {
-    accordionList[0].nextElementSibling.classList.add("ativo");
-
-    function activeAccordion() {
-      this.classList.add("ativo");
-      this.nextElementSibling.classList.toggle("ativo");
-    }
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
-    });
-  }
-}
-
-function initScroll() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    const topo = section.offsetTop;
-    window.scrollTo({
-      top: topo,
-      behavior: "smooth",
-    });
-  }
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
-}
-
-function scrollAnimado() {
-  const sections = document.querySelectorAll(".js-scroll");
-  const windowMetade = window.innerHeight * 0.6;
-
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - windowMetade;
-      if (sectionTop < 0) {
-        section.classList.add("ativo");
-      }
-    });
-  }
-  animaScroll();
-  window.addEventListener("scroll", animaScroll);
-}
-
-initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
 initAccordion();
-initScroll();
-scrollAnimado();
+initTabNav();
